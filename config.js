4a2734f0676bf9ae621687e5c35ce390257f2c3b
DominicDev <s0l0estudi4r@gmail.com>
2026-01-13 13:50:46 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'M83';
+export let nameSongs = [
+	"MIDNIGHT CITY", /*1*/
+	"OUTRO", /*2*/
+	"WAIT", /*3*/
+	"MIDNIGHT CITY' (SLOWED)", /*4*/
+	"OBLIVION", /*5*/
+	"SOLITUDE ", /*6*/
+	"OUTRO - (EXTENDED VERSION)", /*7*/
+	"UN NOUVEAU SOLEIL", /*8*/
+	"I'M SENDING YOU AWAY", /*9*/
+	"LOWER YOUR EYELIDS", /*10*/
+	"THE HIGHEST JOURNEY", /*11*/
+	"COULEURS", /*12*/
+	"MIDNIGHT SOULS STILL REMAIN", /*13*/
+	"GONE", /*14*/
+	"BEAUTIES CAN DIE", /*15*/
+	"SAFE", /*16*/
+	"FAREWELL", /*17*/
+	"TOO LATE", /*18*/
+	"SKIN OF THE NIGHT", /*19*/
+	"SPLENDOR", /*20*/
+]
